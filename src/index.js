e59fcf2bad1f858af4c033af69ceff26569be408
Weeraphat2000 <weeraphat.main@gmail.com>
2024-01-09 11:28:59 +0700

    mui

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import CssBaseline from "@mui/material";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    {/* The rest of your application */}
+    {/* <CssBaseline /> */}
+
     <App />
   </React.StrictMode>
 );
